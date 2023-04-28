99632bc9cb70f89766a7028cc9ad810a3bca5539
Mikebullet-02 <72115697+Mikebullet-02@users.noreply.github.com>
2023-04-27 18:31:27 -0600

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="w-screen sm:w-screen">
+    <div
+      class="bg-inherit py-5 px-6 sm:py-5 sm:px-16 md:py-5 lg:py-8 lg:px-20 xl:px-44 2xl:pb-16 text-start text-neutral-800"
+    >
+      <h1
+        class="sm:mb-5 text-[#003368] text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-6xl font-bold blue:bg-neutral-700 sm:pl-[0px] md:pl-[30px] xl:ml-[10px] tracking-widest ease-in duration-500"
+      >
+        AHORRA HASTA 60% EN
+        <p>LOGÍSTICA PARA TU</p>
+        <p>EMPRESA</p>
+      </h1>
+      <div class="mx-auto max-w-[900px] ml-5 sm:ml-5 md:ml-20 lg:ml-14">
+        <ul
+          class="list-disc flex flex-col space-y-2 py-5 sm:pt-3 md:pt-3 pb-5 text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-2xl sm:mr-5 text-gray-800"
+        >
+          <li>Tarifas preferenciales para empresas</li>
+          <li>
+            Envía y recibe paquetes el mismo día
+            <p class="text-gray-500 inline">(sin costo extra)</p>
+          </li>
+          <li>
+            Cobertura en zonas extendidas
+            <p class="text-gray-500 inline">(sin costo extra)</p>
+          </li>
+          <li>
+            Servicio ocurre, en más de 50 ciudades
+            <p class="text-gray-500 inline">(Bajío, Norte y Centro del país)</p>
+          </li>
+        </ul>
+      </div>
+
+      <div
+        class="mx-auto sm:w-auto sm:pt-[-100px] lg:pt-5 sm:ml-[-30px] sm:py-0 sm:px-20 md:ml-[90px] md:py-15 md:pt-5 md:pl-5 lg:pl-24 2xl:py-16"
+      >
+        <button
+          class="bg-[#003368] h-20 sm:h-20 hover:bg-blue-600 text-white text-xl sm:text-2xl font-extrabold py-2 px-14 sm:py-2 sm:px-18"
+        >
+          <a href="#tarifas">TARIFAS PREFERENCIALES</a>
+        </button>
+      </div>
+      <div
+        class="flex flex-auto mx-auto w-72 sm:w-80 h-auto mr-28 sm:mt-[-600px] opacity-0 2xl:opacity-100 hidden 2xl:block"
+      >
+        <img
+          src="../assets/personajeODM.webp"
+          class="h-[510px] mr-36 w-auto xl:ml-0"
+          onerror="this.src='../assets/personajeODM.png'"
+          alt="Personaje"
+        />
+      </div>
+    </div>
+  </div>
+</template>
